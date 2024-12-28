08d03e3ae8ffec73d7cbc310468da235c6f6db57
Jakob Vogelsang <jakob-vogelsang@posteo.de>
2024-12-28 23:37:50 +0100

    test: some more dump stuff

--- web-test-runner.config.js
+++ web-test-runner.config.js
@@ -23,7 +23,6 @@ const filteredLogs = [
 const browsers = [
      playwrightLauncher({ product: 'chromium' }),
      playwrightLauncher({ product: 'firefox' }),
-     playwrightLauncher({ product: 'webkit' }),
    ];
 
 function defaultGetImageDiff({ baselineImage, image, options }) {
