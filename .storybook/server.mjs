1558f51154773a7e5129e330a6b28de7f8a03907
Jakob Vogelsang <jakob-vogelsang@posteo.de>
2024-12-29 23:15:45 +0100

    refactor: scope element (#3)
    * refactor: scope scl text field
    
    * ci: update screenshots
    
    * docs: readme
    
    * test: remove failed screenshots
    
    * ci: update screenshots
    
    * test: some more shit at the wall throwing
    
    * test: some more shit at the wall throwing
    
    * test: some more dump stuff
    
    * ci: update screenshots
    
    * test: some more dump stuff
    
    * ci: update screenshots
    
    * test: some more tests
    
    * ci: update screenshots
    
    * test: storybook polyfill loading
    
    * ci: update screenshots
    
    * test: improve pipeline
    
    * chore: remove vulnarablities
    
    ---------
    
    Co-authored-by: JakobVogelsang <JakobVogelsang@users.noreply.github.com>

--- .storybook/server.mjs
+++ .storybook/server.mjs
@@ -1,8 +1,13 @@
 import { storybookPlugin } from '@web/dev-server-storybook';
 import baseConfig from '../web-dev-server.config.mjs';
+import { polyfill } from '@web/dev-server-polyfill';
+
 
 export default /** @type {import('@web/dev-server').DevServerConfig} */ ({
   ...baseConfig,
   open: '/',
-  plugins: [storybookPlugin({ type: 'web-components' }), ...baseConfig.plugins],
+  plugins: [
+    polyfill({
+      scopedCustomElementRegistry: true,
+    }),storybookPlugin({ type: 'web-components' }), ...baseConfig.plugins],
 });
